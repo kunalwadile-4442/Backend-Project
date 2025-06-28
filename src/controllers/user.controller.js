7ce7008d9c7c9620d2e37de3de61f68878819848
kunalwadile <kunalwadile12@gmail.com>
2025-06-28 14:35:23 +0530

    fix: correct refresh token validation logic in refreshAccessToken function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -207,7 +207,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
  const incommingRefreshToken =
    req.cookies.refreshToken || req.body.refreshToken;
 
-   if(incommingRefreshToken){
+   if(!incommingRefreshToken){
     throw new ApiError(401, MESSAGES.INVALID_REFRESH_TOKEN);
    }
 
