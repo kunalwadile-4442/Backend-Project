3ddaf0235354827a1d206662de0c9501d25b0135
kunalwadile <kunalwadile12@gmail.com>
2025-06-28 16:51:42 +0530

    fix: format subscription model schema for better readability

--- src/models/subscripption.model.js
+++ src/models/subscripption.model.js
@@ -1,12 +1,18 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-
-const  subscriptionSchema = new Schema({
-  
-    // 8:40
-    // https://youtu.be/9azRerL6CZc?si=wThV3VGTklYFNVjD
-    
-});
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User", // channel who is subscribed
+    },
+  },
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
 
