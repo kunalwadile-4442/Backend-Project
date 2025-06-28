9a5cb2d1052749519eba110c7e585b79693ed288
kunalwadile <kunalwadile12@gmail.com>
2025-06-28 14:39:03 +0530

    feat: add subscription model with mongoose schema stop now

---
+++ src/models/subscripption.model.js
@@ -0,0 +1,12 @@
+import mongoose,{Schema} from "mongoose";
+
+
+const  subscriptionSchema = new Schema({
+  
+    // 8:40
+    // https://youtu.be/9azRerL6CZc?si=wThV3VGTklYFNVjD
+    
+});
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
